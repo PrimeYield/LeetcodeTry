32a76cea007e11abdf56b1096eabdda57e948456
DuckBroApprentice <pigbirthin0320@gmail.com>
2024-11-07 14:29:09 +0800

    229. Majority Element II

--- Array/majorityElementII.go
+++ Array/majorityElementII.go
@@ -1,6 +1,6 @@
+// 229. Majority Element II
 package array
 
-/*
 func majorityElement(nums []int) []int {
 	countMap := make(map[int]int)
 	var res []int
@@ -10,34 +10,6 @@ func majorityElement(nums []int) []int {
 	for j, num := range countMap {
 		if num > len(nums)/3 {
 			res = append(res, j)
-		}
-	}
-	return res
-}
-*/
-
-func majorityElement(nums []int) []int {
-	countMap := make(map[int]int)
-	var res = make([]int, 0, 2)
-	for i := 0; i < len(nums); i++ {
-		countMap[nums[i]]++
-	}
-	if len(countMap) >= (len(nums)-len(nums)/3)+1 {
-		return res
-	}
-	if len(countMap) <= (len(nums)-(len(nums)/3)*2)+1 {
-		for i, num := range countMap {
-			if num > len(nums)/3 {
-				res = append(res, i)
-			}
-		}
-	}
-	if len(countMap) <= (len(nums) - len(nums)/3) {
-		for i, num := range countMap {
-			if num > len(nums)/3 {
-				res = append(res, i)
-				return res
-			}
 		}
 	}
 	return res
