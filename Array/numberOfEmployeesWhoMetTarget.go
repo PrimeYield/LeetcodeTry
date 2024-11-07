8fc065b0b9ac7760d0abb4c00621aaa84dd85c69
DuckBroApprentice <pigbirthin0320@gmail.com>
2024-11-07 15:03:40 +0800

    2798. Number of Employees Who Met the Target

---
+++ Array/numberOfEmployeesWhoMetTarget.go
@@ -0,0 +1,19 @@
+//2798. Number of Employees Who Met the Target
+
+package array
+
+/*
+func numberOfEmployeesWhoMetTarget(hours []int, target int) int {
+	var count int
+	for i := 0; i < len(hours); i++ {
+		if hours[i] >= target {
+			count++
+		}
+	}
+	return count
+}
+*/
+
+func numberOfEmployeesWhoMetTarget(hours []int, target int) int {
+
+}
