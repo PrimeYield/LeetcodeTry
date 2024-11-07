84fcfc1b14035b2519b0f160210c299906b8fef9
DuckBroApprentice <pigbirthin0320@gmail.com>
2024-11-07 14:36:13 +0800

    1351. Count Negative Numbers in a Sorted Matrix

---
+++ Array/countNegativeNumbersInASortedMatrix.go
@@ -0,0 +1,15 @@
+//1351. Count Negative Numbers in a Sorted Matrix
+
+package array
+
+func countNegatives(grid [][]int) int {
+	var count int
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if grid[i][j] < 0 {
+				count++
+			}
+		}
+	}
+	return count
+}
