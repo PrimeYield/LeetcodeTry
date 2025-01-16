f652b0cca9326973548ee9fb33961db958b1829d
DuckBroApprentice <pigbithin0320@gmail.com>
2025-01-17 07:37:21 +0800

    31. Next Permutation

---
+++ Array/nextPermutation.go
@@ -0,0 +1,39 @@
+//31. Next Permutation
+
+func nextPermutation(nums []int) {
+	if len(nums) <= 1 {
+		return
+	}
+
+	i := len(nums) - 1
+	j := len(nums) - 2
+	k := len(nums) - 1
+	for ; ; i, j = i-1, j-1 {
+		if nums[i] > nums[j] {
+			fmt.Println(j)
+			break
+		}
+		if j == 0 && nums[i] < nums[j] {
+			sort.Ints(nums)
+			return
+		}
+		if j == 0 {
+			break
+		}
+	}
+	// if nums[k] <= nums[j] {
+	// 	k--
+	// }
+	for {
+		if nums[k] <= nums[j] {
+			k--
+		} else {
+			break
+		}
+	}
+	fmt.Println(k)
+	nums[j], nums[k] = nums[k], nums[j]
+	//sort[j+1:]
+	sort.Ints(nums[j+1:])
+
+}
