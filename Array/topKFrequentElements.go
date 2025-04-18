b52baebc4a4bcdb115f27beb556b0a6e547abf5e
DuckBroApprentice <pigbirthin0320@gmail.com>
2025-04-18 20:00:00 +0800

    347. Top K Frequent Elements Retry 只是簡化了排序動作

--- Array/topKFrequentElements.go
+++ Array/topKFrequentElements.go
@@ -1,23 +1,60 @@
-//347. Top K Frequent Elements
+// 347. Top K Frequent Elements
 package array
 
+// func topKFrequent(nums []int, k int) []int {
+// 	var res []int
+// 	countMap := make(map[int]int)
+// 	for _, num := range nums {
+// 		countMap[num]++
+// 	}
+// 	for i, _ := range countMap {
+// 		res = append(res, i)
+// 	}
+// 	for i, j := 0, len(res)-1; i < j; j-- {
+// 		if countMap[res[i]] < countMap[res[j]] {
+// 			res[i], res[j] = res[j], res[i]
+// 		}
+// 		if j-i == 1 {
+// 			i++
+// 			j = len(res)
+// 		}
+// 	}
+// 	return res[:k]
+// }
+// Runtime 89 ms
+// Memory 6 MB
 func topKFrequent(nums []int, k int) []int {
-	var res []int
+	if len(nums) <= 1 {
+		return nums
+	}
+	stay := make([][]int, len(nums))
 	countMap := make(map[int]int)
 	for _, num := range nums {
 		countMap[num]++
 	}
-	for i, _ := range countMap {
-		res = append(res, i)
-	}
-	for i, j := 0, len(res)-1; i < j; j-- {
-		if countMap[res[i]] < countMap[res[j]] {
-			res[i], res[j] = res[j], res[i]
+	if len(countMap) == k {
+		var res []int
+		for num, _ := range countMap {
+			res = append(res, num)
 		}
-		if j-i == 1 {
-			i++
-			j = len(res)
+		return res
+	}
+	for num, count := range countMap {
+		stay[count] = append(stay[count], num)
+		//fmt.Println(stay[count], count)
+	}
+	var res []int
+	for i := len(stay) - 1; i >= 0; i-- {
+		for j := 0; j < len(stay[i]); j++ {
+			//fmt.Println(i)
+			if len(res) == k {
+				break
+			}
+			res = append(res, stay[i][j])
 		}
 	}
-	return res[:k]
+	return res
 }
+
+//Runtime 30ms Beats 5.37%
+//Memory 8.29MB Beats 18.15%
