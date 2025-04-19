16eb1dbb57626c3203752f7291d0962569f14fd2
DuckBroApprentice <pigbirthin0320@gmail.com>
2025-04-19 09:26:56 +0800

    stack new try better than old try

--- String/validParentheses.go
+++ String/validParentheses.go
@@ -2,34 +2,67 @@
 
 package string
 
-func close(stack []byte) bool {
-	l := len(stack)
-	if l <= 1 {
-		return false
-	}
-	if stack[l-1] == ')' && stack[l-2] == '(' {
-		return true
-	}
-	if stack[l-1] == ']' && stack[l-2] == '[' {
-		return true
-	}
-	if stack[l-1] == '}' && stack[l-2] == '{' {
-		return true
-	}
+// func close(stack []byte) bool {
+// 	l := len(stack)
+// 	if l <= 1 {
+// 		return false
+// 	}
+// 	if stack[l-1] == ')' && stack[l-2] == '(' {
+// 		return true
+// 	}
+// 	if stack[l-1] == ']' && stack[l-2] == '[' {
+// 		return true
+// 	}
+// 	if stack[l-1] == '}' && stack[l-2] == '{' {
+// 		return true
+// 	}
 
-	return false
-}
+// 	return false
+// }
 
+// func isValid(s string) bool {
+// 	var stack []byte
+// 	for i := 0; i < len(s); i++ {
+// 		stack = append(stack, s[i])
+// 		if s[i] == ')' && close(stack) || s[i] == ']' && close(stack) || s[i] == '}' && close(stack) {
+// 			stack = stack[:len(stack)-2]
+// 		}
+// 	}
+// 	if len(stack) > 0 {
+// 		return false
+// 	}
+// 	return true
+// }
 func isValid(s string) bool {
-	var stack []byte
+	var stack []string
 	for i := 0; i < len(s); i++ {
-		stack = append(stack, s[i])
-		if s[i] == ')' && close(stack) || s[i] == ']' && close(stack) || s[i] == '}' && close(stack) {
-			stack = stack[:len(stack)-2]
+		if string(s[i]) == "(" || string(s[i]) == "[" || string(s[i]) == "{" {
+			stack = append(stack, string(s[i]))
+		}
+		if (string(s[i]) == ")" || string(s[i]) == "]" || string(s[i]) == "}") && len(stack) == 0 {
+			return false
+		}
+		if string(s[i]) == ")" {
+			if stack[len(stack)-1] != "(" {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+		if string(s[i]) == "]" {
+			if stack[len(stack)-1] != "[" {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+		if string(s[i]) == "}" {
+			if stack[len(stack)-1] != "{" {
+				return false
+			}
+			stack = stack[:len(stack)-1]
 		}
 	}
-	if len(stack) > 0 {
-		return false
-	}
-	return true
+	return len(stack) == 0
 }
+
+//Runtime 0ms Beats 100%
+//Memory 4.72MB Beats 9.04%
